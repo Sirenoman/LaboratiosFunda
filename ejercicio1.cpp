4b60e73fc024f350c0ac93c2d50c072fce698f13
sirenoman <00003120@uca.edu.sv>
2020-05-06 11:52:50 -0600

    mi primer programa

---
+++ ejercicio1.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int numero;
+    cout <<"Ingresa un numero por favor "<<endl;
+    cin >> numero;
+    cout << "Su numero es :"<<numero;
+    
+    return 0;
+}
