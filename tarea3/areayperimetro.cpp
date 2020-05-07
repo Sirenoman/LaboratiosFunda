0397c93a5072c266c2f43b5fbc9b7acb7911b507
sirenoman <00003120@uca.edu.sv>
2020-05-06 19:12:22 -0600

    Tarea labo 3

---
+++ tarea3/areayperimetro.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+const double PI  =3.141592653589793238463;
+float radio;
+float per;
+float ar;
+int main()
+{
+    cout << " ¡PROGRAMA PARA CALCULAR AREA Y PERIMETRO DE UN CIRCULO! "<<endl;
+    cout << "Inserte valor del radio que consiguio : ";
+    cin >> radio;
+    per = (2*radio)* PI;
+    ar = PI * pow(radio,2);
+    cout << "El perimetro es: "<<per<<endl;
+    cout << "El area es: "<<ar<<endl;
+
+}
---
+++ tarea3/promedio.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+int num1;
+int num2;
+int num3;
+int promedio;
+
+int main()
+{
+    cout <<"->Programa para sacar promedio de tres numeros Enteros"<<endl;
+    cout << "Inserte primer numero:"; 
+    cin >> num1;
+    cout <<"Inserte segundo numclero: ";
+    cin >> num2;
+    cout <<"Ahora, Tercer numero: ";
+    cin >> num3;
+    promedio = (num1+num2+num3)/3;
+    cout << "Su promedio es: "<<promedio<<endl;
+}
