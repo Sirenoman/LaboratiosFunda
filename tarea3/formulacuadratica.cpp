d56f919b2852bf5bc24cc8b1b1b06e084fc3be80
sirenoman <00003120@uca.edu.sv>
2020-05-06 19:17:37 -0600

    Tarea labo 3

---
+++ tarea3/formulacuadratica.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+int main()
+{
+int a, b, c;
+float x;
+cout << "Tu programa para saber resultado de 3 valores con formula cuadratica: "<<endl;
+cout << "Ingrese a: ";
+cin >> a;
+cout << "Ingrese b: ";
+cin >> b;
+cout << "Ingrese c: ";
+cin >> c;
+
+x = (-b +sqrt(pow(b,2) - 4*a*c))/2*a;
+
+cout <<"El resultado es: ";
+cout << x<<endl;
+return 0;
+}
